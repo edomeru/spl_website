7c41630ac22ce4aa0cc4c0aa9c31c79d25c7b2ed
Edmer <edmer.alarte@bigbenta.com>
2019-01-28 01:12:34 +0900

    messagetest

---
+++ renuale/spl-net.com/images/Old Firefox Data/y52yxp44.default/extensions/{0AA9101C-D3C1-4129-A9B7-D778C6A17F82}/defaults/preferences/rikaichan.js
@@ -0,0 +1,50 @@
+pref('extensions.rikaichan.css', 'blue');
+pref('extensions.rikaichan.enmode', 1);
+pref('extensions.rikaichan.highlight', true);
+pref('extensions.rikaichan.selinlb', true);
+pref('extensions.rikaichan.bottomlb', false);
+pref('extensions.rikaichan.resizedoc', false);
+pref('extensions.rikaichan.sticon', true);
+pref('extensions.rikaichan.minihelp', true);
+
+pref('extensions.rikaichan.cmtoggle', true);
+pref('extensions.rikaichan.cmlbar', false);
+pref('extensions.rikaichan.tmtoggle', true);
+pref('extensions.rikaichan.tmlbar', true);
+
+pref('extensions.rikaichan.nopopkeys', false);
+
+pref('extensions.rikaichan.toggle.key', 'INSERT');
+pref('extensions.rikaichan.toggle.mod', 'alt');
+
+pref('extensions.rikaichan.lbar.key', 'DELETE');
+pref('extensions.rikaichan.lbar.mod', 'alt');
+
+pref('extensions.rikaichan.dpriority', '');
+pref('extensions.rikaichan.wpos', true);
+pref('extensions.rikaichan.wpop', true);
+pref('extensions.rikaichan.wmax', 10);
+pref('extensions.rikaichan.namax', 20);
+
+pref('extensions.rikaichan.kindex', 'COMP,H,L,E,DK,N,V,Y,P,IN,I,U');
+
+pref('extensions.rikaichan.sfile', '');
+pref('extensions.rikaichan.sfcs', 'utf-8');
+pref('extensions.rikaichan.ubom', true);
+pref('extensions.rikaichan.smaxfe', 1);
+pref('extensions.rikaichan.smaxfk', 1);
+pref('extensions.rikaichan.smaxce', 7);
+pref('extensions.rikaichan.smaxck', 1);
+pref('extensions.rikaichan.snlf', 0);
+pref('extensions.rikaichan.ssep', 'Tab');
+
+pref('extensions.rikaichan.version', '');
+
+pref('extensions.rikaichan.popdelay', 50);
+pref('extensions.rikaichan.title', true);
+pref('extensions.rikaichan.dpriority', '');
+pref('extensions.rikaichan.hidedef', false);
+pref('extensions.rikaichan.hidex', false);
+//	pref('extensions.rikaichan.sticky', false);
+
+pref('extensions.rikaichan.firsticon', true);
---
+++ renuale/spl-net.com/js/browserdetect.js
@@ -0,0 +1,21 @@
+﻿		
+
+if (navigator.appName=="Microsoft Internet Explorer")
+{
+	document.getElementById('switchcss').href = 'css/style.css';	
+	
+} 
+else
+{
+	document.getElementById('switchcss').href = 'css/style_others.css';
+}
+
+/*(function(){
+	
+	  var x = document.getElementsByClassName("toshow");
+	  x.classList.add("flip");
+	  x.textContent = "adaafaff";
+	
+	
+	
+}());*/
